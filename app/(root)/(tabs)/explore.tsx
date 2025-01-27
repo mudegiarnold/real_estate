b0c98f3cd1388fc1b1c5e523d6b8ed6bf7a9f2d2
mudegiarnold <mudegi4@gmail.com>
2025-01-27 11:43:54 +0300

    added explore.tsx file

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Explore = () => {
+	  return (
+		      <View>
+		            <Text>Explore</Text>
+			        </View>
+				  );
+};
+
+export default Explore;
+
